34c415ce57856ddc371f889cb27e67514236b913
Stas <stas.kholopkin@gmail.com>
2026-04-05 21:37:05 +0300

    Update main.cpp

--- edit_script_for_BIN/main.cpp
+++ edit_script_for_BIN/main.cpp
@@ -6,31 +6,26 @@
 #include "Data.hpp"
 #include "TelemetryAnalyzer.hpp"
 #include <fstream>
-
 using namespace std;
-// Головна функція реалізації проекту
+
 int main() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    BIN_READER reader("gps.bin"); 
-
-    if (!reader.isOpen()) {
-        cout << "do not open file\n";
+    BIN_READER reader_gps("gps.bin");  
+    if (!reader_gps.isOpen()) {
+        cout << "do not open gps file\n";
         return 1;
     }
 
-    BIN_READER reader("imu.bin"); 
-
-    if (!reader.isOpen()) {
-        cout << "do not open file\n";
+    BIN_READER reader_imu("imu.bin"); 
+    if (!reader_imu.isOpen()) {
+        cout << "do not open imu file\n";
         return 1;
     }
-
 
     Data storage;
-
-    Parser_for_BIN_class::parse(reader, storage);
+    Parser_for_BIN_class::parse(reader_gps, storage);
 
     cout << "GPS points: " << storage.gps.size() << endl;
     cout << "IMU records: " << storage.imu.size() << endl;
@@ -38,12 +33,10 @@ int main() {
     for (const auto& g : storage.gps) {
         cout << "GPS: " << g.altitude << " " << g.latitude << " " << g.longitude << endl;
     }
-
     for (const auto& i : storage.imu) {
         cout << "IMU: " << i.accelX << " " << i.accelY << " " << i.accelZ << endl;
     }
 
-    // Аналіз даних та вивід результатів
     TelemetryAnalyzer analyzer;
     FlightMetrics metrics = analyzer.analyzeTelemetry(storage.gps, storage.imu);
     cout << "Total Distance:   " << metrics.totalDistance << " m" << endl;
@@ -51,14 +44,26 @@ int main() {
     cout << "Max Speed:        " << metrics.maxSpeed << " m/s" << endl;
     cout << "Max Acceleration: " << metrics.maxAcceleration << " m/s^2" << endl;
 
-    //код для запису результатів у JSON файл
+    // Записуємо метрики І GPS точки у JSON
     ofstream json_file("metrics.json");
     json_file << "{\n";
     json_file << "  \"total_distance\": " << metrics.totalDistance << ",\n";
     json_file << "  \"flight_duration\": " << metrics.flightDuration << ",\n";
     json_file << "  \"max_speed\": " << metrics.maxSpeed << ",\n";
-    json_file << "  \"max_acceleration\": " << metrics.maxAcceleration << "\n";
+    json_file << "  \"max_acceleration\": " << metrics.maxAcceleration << ",\n";
+    json_file << "  \"gps_points\": [\n";
+    for (size_t i = 0; i < storage.gps.size(); i++) {
+        json_file << "    {";
+        json_file << "\"lat\": " << storage.gps[i].latitude << ", ";
+        json_file << "\"lon\": " << storage.gps[i].longitude << ", ";
+        json_file << "\"alt\": " << storage.gps[i].altitude;
+        json_file << "}";
+        if (i < storage.gps.size() - 1) json_file << ",";
+        json_file << "\n";
+    }
+    json_file << "  ]\n";
     json_file << "}\n";
     json_file.close();
+
     return 0;
 }
