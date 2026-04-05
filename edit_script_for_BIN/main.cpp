562c0db0e1803658aef7eb91f5fac655e7e963aa
Stas <stas.kholopkin@gmail.com>
2026-04-05 20:59:56 +0300

    Update main.cpp

--- edit_script_for_BIN/main.cpp
+++ edit_script_for_BIN/main.cpp
@@ -5,6 +5,7 @@
 #include "parser_for_BIN_class.hpp"
 #include "Data.hpp"
 #include "TelemetryAnalyzer.hpp"
+#include <fstream>
 
 using namespace std;
 // Головна функція реалізації проекту
@@ -34,12 +35,22 @@ int main() {
     //     cout << "IMU: " << i.accelX << " " << i.accelY << " " << i.accelZ << endl;
     // }
 
+    // Аналіз даних та вивід результатів
     TelemetryAnalyzer analyzer;
     FlightMetrics metrics = analyzer.analyzeTelemetry(storage.gps, storage.imu);
     cout << "Total Distance:   " << metrics.totalDistance << " m" << endl;
     cout << "Flight Duration:  " << metrics.flightDuration << " s" << endl;
     cout << "Max Speed:        " << metrics.maxSpeed << " m/s" << endl;
     cout << "Max Acceleration: " << metrics.maxAcceleration << " m/s^2" << endl;
-    
+
+    //код для запису результатів у JSON файл
+    ofstream json_file("metrics.json");
+    json_file << "{\n";
+    json_file << "  \"total_distance\": " << metrics.totalDistance << ",\n";
+    json_file << "  \"flight_duration\": " << metrics.flightDuration << ",\n";
+    json_file << "  \"max_speed\": " << metrics.maxSpeed << ",\n";
+    json_file << "  \"max_acceleration\": " << metrics.maxAcceleration << "\n";
+    json_file << "}\n";
+    json_file.close();
     return 0;
 }
